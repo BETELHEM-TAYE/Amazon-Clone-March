5889a55f0df49b9f7be8a966122ac028d5cee6b1
gbeti7669 <gbeti7669@gmail.com>
2025-09-23 22:22:35 +0000

    configuration update

--- functions/index.js
+++ functions/index.js
@@ -1,23 +1,18 @@
-/* eslint-env node */
-/* eslint-disable no-undef */
-const { onRequest } = require("firebase-functions/v2/https");
+
+const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
-
 const stripe = require("stripe")(process.env.STRIPE_KEY);
-
 const app = express();
-app.use(cors({ origin: true }));
+app.use(cors({origin: true}));
 app.use(express.json());
-
 app.get("/", (req, res) => {
   res.status(200).json({
     message: "Success!",
   });
 });
-
 app.post("/payment/create", async (req, res) => {
   const total = parseInt(req.query.total);
   if (total > 0) {
@@ -28,12 +23,10 @@ app.post("/payment/create", async (req, res) => {
     res.status(201).json({
       clientSecret: paymentIntent.client_secret,
     });
-    console.log(paymentIntent.client_secret);
   } else {
     res.status(403).json({
-      message: "Total must be greater than 0",
+      message: "total must be greater than 0",
     });
   }
 });
-
-exports.api = onRequest(app);
+exports.api = functions.https.onRequest(app);
--- src/Api/axios.jsx
+++ src/Api/axios.jsx
@@ -1,5 +1,6 @@
-import axios from"axios"
+
+import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: "https://amazon-api-deploy-jf50.onrender.com",
+  baseURL: "http://127.0.0.1:5001/clone-70822/us-central1/api",
 });
 export {axiosInstance};
