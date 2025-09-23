91f4fc1c463ab415f97f6c2972f3d550b810050d
BETELHEM-TAYE <gbeti7669@gmail.com>
2025-09-23 13:46:14 -0700

    configure backend

--- amazon-clone/functions/index.js
+++ amazon-clone/functions/index.js
@@ -1,14 +1,11 @@
-
-
-const {onRequest} = require("firebase-functions/v2/https");
+const { onRequest } = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const stripe = require("stripe")(process.env.STRIPE_KEY)
-
+const stripe = require("stripe")(process.env.STRIPE_KEY);
 
 const app = express();
 app.use(cors({ origin: true }));
@@ -30,7 +27,7 @@ app.post("/payment/create", async (req, res) => {
     res.status(201).json({
       clientSecret: paymentIntent.client_secret,
     });
-    console.log(clientSecret)
+    console.log(clientSecret);
   } else {
     res.status(403).json({
       message: "Total must be greater than 0",
@@ -38,5 +35,4 @@ app.post("/payment/create", async (req, res) => {
   }
 });
 
-
-exports.api=onRequest(app)
+exports.api = onRequest(app);
