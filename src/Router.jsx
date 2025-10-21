cfd6e2114c3f452f8142a44856d46c9d92d551a6
BETELHEM-TAYE <gbeti7669@gmail.com>
2025-10-21 15:54:07 -0700

    update stripe public key

--- src/Router.jsx
+++ src/Router.jsx
@@ -11,7 +11,7 @@ import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import ProtectedRoute from "./Components/ProtectRoute/ProtectedRoute";
 const stripePromise = loadStripe(
-  "pk_test_51Pbi87RoCo5zS2p9FwsPJ21REkfG5p8fR1CjpdB259g6EClgXfF9aJ4jywL5mDb0GkKqO6N2yZ3c3aJ4N0b6g0b00Cg3Z3S0A"
+  "pk_test_51SAz3WGhm2eRUyYiXRvatKs1HPgmlhVAgmvSFLijKXSYSxYWyAo152ttrgc4XNEGobLHqrAxX8XnQ7ePF7O7t1je00nodNDNnE"
 );
 
 function Routering() {
